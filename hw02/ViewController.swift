546e38317f585aad1347b06f5bbab29a598988ff
david <hurling0@gmail.com>
2024-10-01 10:12:05 +0800

    Initial Commit

---
+++ hw02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw02
+//
+//  Created by MacBookPro2020 on 2024/10/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
